67e95ee52ee9cb7cf8f4b4c35d6de4e7e515777d
Melanie Adler <adl.melanie.95@gmail.com>
2024-08-16 10:45:56 +0200

    create first card

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,17 +1,17 @@
+import { initialColors } from "../../lib/colors";
 import "./Color.css";
 
-export default function Color({ color }) {
+export default function Color() {
   return (
-    <div
-      className="color-card"
-      style={{
-        background: color.hex,
-        color: color.contrastText,
-      }}
-    >
-      <h3 className="color-card-headline">{color.hex}</h3>
-      <h4>{color.role}</h4>
-      <p>contrast: {color.contrastText}</p>
-    </div>
+    <>
+      <div
+        className="color-card"
+        style={{ backgroundColor: `${initialColors[0].hex}` }}
+      >
+        <h2 className="color-card-headline">{initialColors[0].hex}</h2>
+        <p>{initialColors[0].role}</p>
+        <p>contrast: {initialColors[0].contrastText}</p>
+      </div>
+    </>
   );
 }
